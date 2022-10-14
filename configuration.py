830e348cec8f34423458b0387b675a3219c22a9b
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2022-10-15 08:07:14 +0900

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,8 +1,4 @@
-# dataset_name = "mnist"
-# dataset_name = "fashion_mnist"
-dataset_name = "voc"
-# dataset_name = "coco"
-# dataset_name = "OID_v6"
+dataset_name = "OID_v6"
 
 TRAIN_FROM_CHECKPOINT       = False # "saved_model/yolov3_custom"
 TRAIN_YOLO_TINY             = False
@@ -20,61 +16,16 @@ YOLO_V4_WEIGHTS             = "./checkpoints/yolov4.weights"
 YOLO_V4_TINY_WEIGHTS        = "./checkpoints/yolov4-tiny.weights"
 # YOLO_TRT_QUANTIZE_MODE      = "INT8" # INT8, FP16, FP32
 YOLO_COCO_CLASSES           = "./dataset/coco.names"
-    
-if dataset_name == "mnist":
-    TRAIN_CLASSES               = "./dataset/mnist/mnist.names"
-    TRAIN_ANNOT_PATH            = "./dataset/mnist/mnist_train.txt"
-    TEST_ANNOT_PATH             = "./dataset/mnist/mnist_test.txt"
-    if YOLO_TYPE == "yolov3":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints"
-        DATA_TYPE = "yolo_v3_mnist"
-    elif YOLO_TYPE == "yolov4":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints"
-        DATA_TYPE = "yolo_v4_mnist"
 
-elif dataset_name == "fashion_mnist":
-    TRAIN_CLASSES               = "./dataset/fashion_mnist/mnist.names"
-    TRAIN_ANNOT_PATH            = "./dataset/fashion_mnist/mnist_train.txt"
-    TEST_ANNOT_PATH             = "./dataset/fashion_mnist/mnist_val.txt"
-    if YOLO_TYPE == "yolov3":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints"
-        DATA_TYPE = "yolo_v3_fashion_mnist"
-    elif YOLO_TYPE == "yolov4":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints"
-        DATA_TYPE = "yolo_v4_fashion_mnist"
-
-elif dataset_name == "voc":
-    TRAIN_CLASSES               = "./dataset/voc/voc2012.names"
-    TRAIN_ANNOT_PATH            = "./dataset/voc/VOC2012_train.txt"
-    TEST_ANNOT_PATH             = "./dataset/voc/VOC2012_val.txt"
-    if YOLO_TYPE == "yolov3":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints/yolo_v3_voc"
-        DATA_TYPE = "yolo_v3_voc"
-    elif YOLO_TYPE == "yolov4":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints/yolo_v4_voc"
-        DATA_TYPE = "yolo_v4_voc"
-    
-elif dataset_name == "coco":
-    TRAIN_CLASSES               = "./dataset/coco/coco.names"
-    TRAIN_ANNOT_PATH            = "./dataset/coco/COCO2017_train.txt"
-    TEST_ANNOT_PATH             = "./dataset/coco/COCO2017_val.txt"
-    if YOLO_TYPE == "yolov3":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints/yolo_v3_coco"
-        DATA_TYPE = "yolo_v3_coco"
-    elif YOLO_TYPE == "yolov4":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints/yolo_v4_coco"
-        DATA_TYPE = "yolo_v4_coco"
-        
-elif dataset_name == "OID_v6":
-    TRAIN_CLASSES               = "./dataset/OID_v6/OID_V6.names"
-    TRAIN_ANNOT_PATH            = "./dataset/OID_v6/OID_V6_train.txt"
-    TEST_ANNOT_PATH             = "./dataset/OID_v6/OID_V6_test.txt"
-    if YOLO_TYPE == "yolov3":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints/yolo_v3_OID_v6"
-        DATA_TYPE = "yolo_v3_OID_v6"
-    elif YOLO_TYPE == "yolov4":
-        TRAIN_CHECKPOINTS_FOLDER    = "./checkpoints/yolo_v4_OID_v6"
-        DATA_TYPE = "yolo_v4_OID_v6"
+TRAIN_CLASSES               = "./dataset/OID_v6/OID_V6.names"
+TRAIN_ANNOT_PATH            = "./dataset/OID_v6/OID_V6_train.txt"
+TEST_ANNOT_PATH             = "./dataset/OID_v6/OID_V6_test.txt"
+if YOLO_TYPE == "yolov3":
+    TRAIN_CHECKPOINTS_FOLDER = "./checkpoints/yolo_v3_OID_v6"
+    DATA_TYPE = "yolo_v3_OID_v6"
+elif YOLO_TYPE == "yolov4":
+    TRAIN_CHECKPOINTS_FOLDER = "./checkpoints/yolo_v4_OID_v6"
+    DATA_TYPE = "yolo_v4_OID_v6"
 
 if TRAIN_YOLO_TINY:
     TRAIN_CHECKPOINTS_FOLDER  += "_tiny"
